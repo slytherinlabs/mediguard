14a3c713c0dfbfd28c494ed793f6a2ea01035d37
rahulkpr2510 <rahulkpr2510@gmail.com>
2026-04-21 19:20:06 +0530

    feat: initialize Hardhat configuration with Solidity 0.8.24 and network settings

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL ?? "",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+    },
+  },
+};
+
+export default config;
